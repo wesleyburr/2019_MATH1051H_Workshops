3e1c432ccfbab791f13e48b3b23b7d80f0c9f653
Wesley Burr <wesley.burr@gmail.com>
2018-09-12 20:12:53 -0400

    Workshop 02

---
+++ R_Packages.R
@@ -0,0 +1,21 @@
+install.packages(c("assertthat", "backports", "base64enc", "BH", "bindr", "bindrcpp", 
+                   "broom", "callr", "cellranger", "cli", "clipr", "colorspace", "crayon", 
+                   "crosstalk", "curl", "datasauRus", "DBI", "dbplyr", "dichromat", "digest", 
+                   "dplyr", "DT", "evaluate", "forcats", "ggplot2", "glue", "gtable", "haven", 
+                   "highr", "hms", "htmltools", "htmlwidgets", "httpuv", "httr", "jsonlite", 
+                   "knitr", "labeling", "later", "lazyeval", "lubridate", "magrittr", "markdown", 
+                   "mime", "mnormt", "modelr", "munsell", "openssl", "pillar", "pkgconfig", 
+                   "plogr", "plyr", "praise", "processx", "promises", "psych", "purrr",      
+                   "R6", "RColorBrewer", "Rcpp", "readr", "readxl", "rematch", "reprex", "reshape3",   
+                   "rlang",  "rmarkdown", "rprojroot", "rstudioapi", "rvest",  "scales", "selectr", "shiny",      
+                   "sourcetools", "stringi", "stringr", "testthat", "tibble", "tidyr", "tidyselect", "tidyverse",  
+                   "tinytex", "unvotes", "utf8", "viridisLite", "whisker", "withr", "xml3", "xtable",     
+                   "yaml", "base", "boot", "class", "cluster", "codetools", "compiler", "datasets",   
+                   "foreign", "graphics", "grDevices", "grid", "KernSmooth", "lattice", "MASS", "Matrix",     
+                   "methods", "mgcv", "nlme", "nnet", "parallel", "rpart", "spatial", "splines",    
+                   "stats", "stats4", "survival", "tcltk", "tools", "utils"))
+
+
+
+
+
